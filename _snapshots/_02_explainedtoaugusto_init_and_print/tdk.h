c530f528b1cc8d3ff03c2222fb70e18317e3b8fc
hecastan <hecastan@student.42sp.org.br>
2023-03-12 13:02:12 -0300

    snapshot02and aw

---
+++ _snapshots/_02_explainedtoaugusto_init_and_print/tdk.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tdk.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hecastan <hecastan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/11 12:26:44 by hecastan          #+#    #+#             */
+/*   Updated: 2023/03/12 12:21:54 by hecastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TDK_H
+# define TDK_H
+
+#include <unistd.h>
+
+typedef struct s_pov
+{
+	char	up[4];
+	char	down[4];
+	char	left[4];
+	char	right[4];	
+}
+t_pov;
+
+int		ft_error(void);
+int		ft_parser(char *argv, t_pov *pov);
+int		ft_init_grid(char grid[4][4]);
+int		ft_solve(char grid[4][4], t_pov *pov);
+int		ft_print_grid(char grid[4][4]);
+void	ft_putstr(char *str);
+
+
+#endif
---
+++ _snapshots/_02_explainedtoaugusto_init_and_print/tdk00.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tdk00.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hecastan <hecastan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/12 08:36:37 by hecastan          #+#    #+#             */
+/*   Updated: 2023/03/12 12:58:32 by hecastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tdk.h"
+
+int	ft_error(void)
+{
+	write(1, "Error\n", 6);
+	return (0);
+}
+
+int	ft_parser(char *argv, t_pov *pov)
+{
+/*
+** 	int		i;
+** 	char	*str_ptr_pov;
+** 
+** 	i = 0;
+** 	str_ptr_pov = &pov->up[0];
+** 	while (argv[i] != '\0')
+** 	{
+** 		if ((i % 2 != 0) && (argv[i] == ' '))
+** 			i += 0;
+** 		else if ((i % 2 == 0) && (argv[i] >= 1 && argv[i] <= 4))
+** 			*str_ptr_pov++ = argv[i] - '0';
+** 		else
+** 			return (1);
+** 		i++;
+** 	}
+** 	if (i != 32)
+** 		return (1);
+** 	ft_putstr(str_ptr_pov);
+*/
+	return (0);
+}
+
+int	ft_init_grid(char grid[4][4])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			grid[i][j] = '0';
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	ft_solve(char grid[4][4], t_pov *pov)
+{
+	return (0);
+}
+
+int	ft_print_grid(char grid[4][4])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			//grid[i][j] += '0';
+			write(1, &grid[i][j], 1);
+			//grid[i][j] -= '0';
+			if (j != 3)
+				write(1, " ", 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
