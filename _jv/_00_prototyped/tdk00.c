c3dba8e60dd572e9738a387a61bfb9256f014baa
hecastan <hecastan@student.42sp.org.br>
2023-03-12 09:21:51 -0300

    snapshot copies to individual repos

---
+++ _jv/_00_prototyped/tdk00.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tdk00.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hecastan <hecastan@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/12 08:36:37 by hecastan          #+#    #+#             */
+/*   Updated: 2023/03/12 09:11:13 by hecastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tdk.h"
+
+int	ft_error(void)
+{
+	write(1, "Error\n", 6);
+	return (0);
+}
+
+int	ft_parser(char **grid, t_pov *pov)
+{
+	return (0);
+}
+
+int	ft_init_grid(char **grid)
+{
+	return (0);
+}
+
+int	ft_solve(char **grid, t_pov *pov)
+{
+	return (0);
+}
+
+int	ft_print_grid(char **grid)
+{
+	return (0);
+}
